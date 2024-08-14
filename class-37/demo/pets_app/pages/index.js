f325379aae2ca123bafdbfc4dd705529b6bee78f
roaaabualeeqa <roaa.abualeeqa@gmail.com>
2024-08-14 09:08:13 +0000

    class-37 done

---
+++ class-37/demo/pets_app/pages/index.js
@@ -0,0 +1,14 @@
+'use client';
+import Header from '../components/Header'
+import Hero from '@/components/Hero';
+import Form from '@/components/Form';
+
+export default function Page(){
+  return (
+    <>
+    <Header />
+    <Hero />
+    <Form />
+    </>
+  )
+}
